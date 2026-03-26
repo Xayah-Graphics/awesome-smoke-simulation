0889efb4895fb65b15f18c2e86882b8c9ea677c5
Xayah-Hina <ycxiang14sep@gmail.com>
2026-03-26 20:01:21 +0800

    Add custom cylinder collider support and update collider handling in stable fluids

--- app/stable_simulation.hpp
+++ app/stable_simulation.hpp
@@ -29,7 +29,7 @@ namespace smoke {
         float center_x        = 0.50f;
         float center_y        = 0.50f;
         float center_z        = 0.50f;
-        float radius          = 0.125f;
+        float radius          = 0.25f;
         float half_extent_x   = 0.10f;
         float half_extent_y   = 0.08f;
         float half_extent_z   = 0.10f;
